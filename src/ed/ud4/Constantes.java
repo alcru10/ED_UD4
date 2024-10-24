ba3034c9adbab29901170d7ede77a11bc30e9d32
Hector Martinez <hectoralcru@gmail.com>
2024-10-24 17:26:37 +0200

    cambios

---
+++ src/ed/ud4/Constantes.java
@@ -0,0 +1,14 @@
+package ed.ud4;
+
+public class Constantes {
+    //Fuerza de la Gravezad g
+    public static final double G = 9.80665;
+
+    public static void main (String[] args) {
+        //Con que fuerza atrae la tierra a un balón de fútbol
+        //Fórmula: F = m * g;
+        double m = .450;
+        double f = m *  G;
+        System.out.println("La tierra atrae a un balón de fútbol con una fuerza de " + f + " newtons");
+    }
+}
