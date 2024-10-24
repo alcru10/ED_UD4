d05f88bb918276b72e7968167d24f8b5ecc62532
Hector Martinez <hectoralcru@gmail.com>
2024-10-24 16:55:48 +0200

    nombre

--- src/ed/ud4/Average.java
+++ src/ed/ud4/Average.java
@@ -1,8 +1,9 @@
 package ed.ud4;
-public class App {
+public class Average {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         //4
-        
+        double media = (double) total / (double) count;
+        System.out.println("Media: " + media);
     }
 }
