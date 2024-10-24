488c2f5fdf2f1cf82a065204ea9838196bdf862d
Hector Martinez <hectoralcru@gmail.com>
2024-10-24 16:53:30 +0200

    Nueva clase

--- src/ed/ud4/App.java
+++ src/ed/ud4/App.java
@@ -3,5 +3,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         //4
+        double media = (double) total / (double) count;
+        System.out.println("Media: " + media);
     }
 }
